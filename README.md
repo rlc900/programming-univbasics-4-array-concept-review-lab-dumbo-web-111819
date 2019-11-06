52a86aa9c6d043b06d4f37a1447e6e7b24421d20
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-06 00:33:33 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Using Loops and Arrays to Find Data
+w# Using Loops and Arrays to Find Data
 
 ## Learning Goals
 
--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  i = 0 
+  while i < array.length do 
+    return array.index(value_to_find)
+    i += 1 
+  end 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  
 end
