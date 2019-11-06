6eb98599d4864deb957eb55efef18e9485896d0e
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-06 00:34:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,10 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  i = 0 
+  while i < array.length do 
+    return array.max
+    i += 1 
 end
 
 def find_min_value(array)
